2729a378e492fc10b4b295253b840d403f64f91d
Dulio Denis <dulio.denis@yahoo.com>
2015-05-31 12:11:31 -0400

    Added a MainWindowController with XIB

---
+++ PasswordGenerator/PasswordGenerator/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  PasswordGenerator
+//
+//  Created by Dulio Denis on 5/30/15.
+//  Copyright (c) 2015 Dulio Denis. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
